20c82a2e1177f8beb1b1b75e137ce75d8db264f9
gamejoye <gamejoye@gmail.com>
2024-06-28 13:51:39 +0800

    feat(utils): http autoretry

---
+++ utils/http.ts
@@ -0,0 +1,23 @@
+export async function fetchWithRetry<T>(
+  callback: () => (Promise<T>),
+  isSuccess: (arg: T) => boolean,
+  retries = 3,
+  delay = 500,
+): Promise<T> {
+  try {
+    const response = await callback();
+    if (isSuccess(response)) return response;
+    if (!isSuccess(response) && retries > 0) {
+      await new Promise(resolve => setTimeout(resolve, delay));
+      return fetchWithRetry(callback, isSuccess, retries - 1, delay);
+    }
+    throw new Error('网络开了一点小差～');
+  } catch (error) {
+    if (retries > 0) {
+      console.log(`尝试重新请求 ${retries}`);
+      await new Promise(resolve => setTimeout(resolve, delay));
+      return fetchWithRetry(callback, isSuccess, retries - 1, delay);
+    }
+    throw error;
+  }
+}
