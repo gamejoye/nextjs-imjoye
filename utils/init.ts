2aeaa9269552895f3b8f10230440533a85adcb28
gamejoye <gamejoye@gmail.com>
2024-06-28 13:52:14 +0800

    feat(utils): init imjc module

--- utils/init.ts
+++ utils/init.ts
@@ -1,6 +1,16 @@
 import appServerApi from "@/api/appServerApi";
 import { WebAppServerApiImpl } from "@/api/web/webAppServerApi.impl";
+import eventEmitter from "@/imjc/emitter";
+import imjcManager from "@/imjc/imjc";
+import WebEventEmitter from "@/imjc/web/emitter.proto";
+import WebIMJCManagerImpl from "@/imjc/web/imjc.proto";
 
 export function initServer() {
   appServerApi.init(new WebAppServerApiImpl());
+  eventEmitter.init(new WebEventEmitter());
+  const webImjcManager = new WebIMJCManagerImpl(
+    appServerApi,
+    eventEmitter,
+  );
+  imjcManager.init(webImjcManager);
 }
