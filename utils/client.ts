100a59a99cc3898704bdedcbbd29e367d7407b5b
gamejoye <gamejoye@gmail.com>
2024-07-01 00:52:35 +0800

    feat(imjc): websocket communication

--- utils/client.ts
+++ utils/client.ts
@@ -5,13 +5,16 @@ import eventEmitter from "@/imjc/emitter";
 import imjcManager from "@/imjc/imjc";
 import WebEventEmitter from "@/imjc/web/emitter.proto";
 import WebIMJCManagerImpl from "@/imjc/web/imjc.proto";
+import { UserInfoUtil } from './userInfo';
 
 export function initClient() {
-  appServerApi.init(new WebAppServerApiImpl(-1, ''));
+  const { userId, authenticatedToken } = UserInfoUtil.getUserInfo();
+  appServerApi.init(new WebAppServerApiImpl(userId, authenticatedToken));
   eventEmitter.init(new WebEventEmitter());
   const webImjcManager = new WebIMJCManagerImpl(
     appServerApi,
     eventEmitter,
   );
   imjcManager.init(webImjcManager);
+  imjcManager.connect(userId, authenticatedToken);
 }
