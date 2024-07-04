4a46e3d788a67ad0e376b0934489b0c0e4629f5c
gamejoye <gamejoye@gmail.com>
2024-07-04 21:37:07 +0800

    feat(utils): showRelativeTime

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -1,17 +1,23 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 export const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+export const DATE_FORMAT = 'YYYY-MM-DD';
 
-export function formatDate(date: Date) {
-  const year = date.getFullYear();
-  // getMonth() 返回的月份从0开始，所以需要+1
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
+dayjs.extend(relativeTime);
 
 export function getCurrentDatetime() {
-  return formatDate(new Date());
+  return dayjs().format(DATETIME_FORMAT);
+}
+
+export function formatDate(datetime: string | Date) {
+  return dayjs(datetime).format(DATE_FORMAT);
+}
+
+export function formatDatetime(datetime: string | Date) {
+  return dayjs(datetime).format(DATETIME_FORMAT);
+}
+
+export function showRelativeTime(datetime: string | Date) {
+  return dayjs(datetime).fromNow();
 }
