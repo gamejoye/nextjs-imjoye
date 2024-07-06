3fd0a22a9cb17a2a1a7d9f83924c27dab817a25d
gamejoye <gamejoye@gmail.com>
2024-07-06 16:06:43 +0800

    feat(pages): friend page provide user online search

--- app/contact/friend/page.tsx
+++ app/contact/friend/page.tsx
@@ -1,7 +1,8 @@
 import { FriendList } from "@/component/FriendList";
+import { FriendOnlineSearch } from "@/component/FriendOnlineSearch";
 import imjcManager from "@/imjc/imjc";
 import { getUserInfo, initServer } from "@/utils/server";
-import { message } from "antd";
+import { Tabs, message } from "antd";
 import { Content } from "antd/es/layout/layout";
 
 export default async function Friend() {
@@ -10,9 +11,18 @@ export default async function Friend() {
   const friends = await imjcManager.getFriendsFromRemote(userId, (err) => {
     message.error('获取好友信息失败:' + err.message);
   });
+  const items = [{
+    label: `全部`,
+    key: '0',
+    children: <FriendList friends={friends} />,
+  }]
   return (
-    <Content>
-      <FriendList friends={friends} />
-    </Content>
+    <Content style={{ padding: 8 }}>
+      <Tabs
+        type={'card'}
+        items={items}
+        tabBarExtraContent={<FriendOnlineSearch friends={friends} />}
+      />
+    </Content >
   );
 }
