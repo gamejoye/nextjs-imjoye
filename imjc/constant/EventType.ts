ef3fa946f460f63ec835244d10f8435805f6fa37
gamejoye <gamejoye@gmail.com>
2024-07-24 18:37:14 +0800

    feat(imjc): new EventType

--- imjc/constant/EventType.ts
+++ imjc/constant/EventType.ts
@@ -1,5 +1,8 @@
 export class EventType {
-  static NEW_MESSAGE = 1;       // 新消息
-  static JOIN_CHATROOM = 2;     // 加入聊天室
-  static QUIT_CHATROOM = 3;     // 退出聊天室
+  static NEW_MESSAGE = 1;         // 新消息
+  static JOIN_CHATROOM = 2;       // 加入聊天室
+  static QUIT_CHATROOM = 3;       // 退出聊天室
+  static NEW_FRIEND_REQUEST = 4;  // 新的好友请求
+  static NEW_CHATROOM = 5;        // 新的聊天室
+  static NEW_FRIEND = 6;          // 新的好友
 }
