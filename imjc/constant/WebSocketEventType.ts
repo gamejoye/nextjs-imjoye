46ea77ba89e372d51572392129f9c3a34a0b4480
gamejoye <gamejoye@gmail.com>
2024-07-24 18:36:59 +0800

    feat(imjc): new WebSocketEventType

--- imjc/constant/WebSocketEventType.ts
+++ imjc/constant/WebSocketEventType.ts
@@ -1,3 +1,6 @@
 export class WebSocketEventType {
-  static NEW_MESSAGE = 1;     // 新消息
+  static NEW_MESSAGE = 1;         // 新消息
+  static NEW_FRIEND_REQUEST = 2;  // 新的好友请求
+  static NEW_FRIEND = 3;          // 新的好友
+  static NEW_CHATROOM = 4;        // 新的聊天室
 }
