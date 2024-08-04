92353e5fca32b6f59634dfca034f4e426d78b7cb
gamejoye <gamejoye@gmail.com>
2024-08-04 20:44:56 +0800

    feat(constant): CONNECTION_STATUS_CHANGED

--- imjc/constant/EventType.ts
+++ imjc/constant/EventType.ts
@@ -1,8 +1,9 @@
 export class EventType {
-  static NEW_MESSAGE = 1;         // 新消息
-  static JOIN_CHATROOM = 2;       // 加入聊天室
-  static QUIT_CHATROOM = 3;       // 退出聊天室
-  static NEW_FRIEND_REQUEST = 4;  // 新的好友请求
-  static NEW_CHATROOM = 5;        // 新的聊天室
-  static NEW_FRIEND = 6;          // 新的好友
+  static NEW_MESSAGE = 1;               // 新消息
+  static JOIN_CHATROOM = 2;             // 加入聊天室
+  static QUIT_CHATROOM = 3;             // 退出聊天室
+  static NEW_FRIEND_REQUEST = 4;        // 新的好友请求
+  static NEW_CHATROOM = 5;              // 新的聊天室
+  static NEW_FRIEND = 6;                // 新的好友
+  static CONNECTION_STATUS_CHANGED = 7; // 连接状态改变
 }
